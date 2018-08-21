f748a52bc06c0faa42d4ccaac371e3bd3a26e40f
Nick Sanford <nicholascsanford@gmail.com>
2018-08-21 16:43:29 -0400

    Add ghc-events & threadscope to event log

--- profile.sh
+++ profile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+stack install ghc-events
 stack clean
 stack build --profile
-rm wc-l-exe.eventlog wc-l-exe.dashs wc-l-exe.prof
-stack exec -- wc-l-exe ../concurrent-wc/tmp/ +RTS -l # generate eventlog
-stack exec -- wc-l-exe ../concurrent-wc/tmp/ +RTS -p # generate profile
-stack exec -- wc-l-exe ../concurrent-wc/tmp/ +RTS -s 2> wc-l-exe.dashs
-stack exec -- wc-l-exe ../concurrent-wc/tmp/ > wc-l-exe.output
+rm wc-l-exe.eventlog wc-l-exe.dashs wc-l-exe.prof wc-l-ghc-events.output
+stack exec -- wc-l-exe ./tmp/ +RTS -l # generate eventlog
+stack exec -- wc-l-exe ./tmp/ +RTS -p # generate profile
+stack exec -- wc-l-exe ./tmp/ +RTS -s 2> wc-l-exe.dashs
+stack exec -- wc-l-exe ./tmp/ > wc-l-exe.output
+stack exec ghc-events show wc-l-exe.eventlog > wc-l-ghc-events.output
+threadscope wc-l-exe.eventlog
